36153da60c06cef99dca13fdee11f1d12e4c1387
Ammar2j <ammarahmed89754@gmail.com>
2020-10-07 16:07:34 +0500

    Add method added with filing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 import java.io.IOException;
 import java.util.Date;
 import java.util.Scanner;
@@ -9,8 +8,15 @@ class bus {
     public String busName;
     public int busNumber;
 
-    public void addBus(){
 
+    public bus(int busId,String busName, int busNumber){
+        this.busId = busId;
+        this.busName = busName;
+        this.busNumber = busNumber;
+    }
+
+    public void addBus(PrintWriter write){
+        write.print("Bus Id: " + busId + " Bus Name: " + busName + " Bus Number: "+ busNumber);
     }
 
     public void deleteBus(){
@@ -33,8 +39,15 @@ class customer {
     public int customerPhone;
     public String customerAddr;
 
-    public void addCustomer(){
+    public customer(int customerId, String customerName, int customerPhone, String customerAddr){
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerPhone = customerPhone;
+        this.customerAddr = customerAddr;
+    }
 
+    public void addCustomer(PrintWriter write1){
+        write1.print("Customer Id: " + customerId + " Customer Name: " + customerName + " Customer Phone: "+ customerPhone + " Customer Address: " + customerAddr);
     }
 
     public void deleteCustomer(){
@@ -79,11 +92,65 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 	// write your code here
-        File file = new File("/home/ammar/Documents/OOP_Project");
-        FileWriter write = new FileWriter(file);
+        File file = new File("/home/ammar/Documents/OOP_Project/testbus.txt");
+        File file1 = new File("/home/ammar/Documents/OOP_Project/testcust.txt");
+        PrintWriter write = new PrintWriter(file);
+        PrintWriter write1 = new PrintWriter(file1);
 
-        System.out.println("Press 1 for Adding bus \nPress 2 for Adding customer \nPress 0 to Exit");
-        Scanner ip = new Scanner(System.in);
-        
+        boolean exit = false;
+        while (exit == false) {
+
+            System.out.println("Press 1 for Adding bus \nPress 2 for Adding customer \nPress 0 to Exit");
+            Scanner ip = new Scanner(System.in);
+            int selectioninput = ip.nextInt();
+
+            if (selectioninput == 1) {
+                int busId;
+                String busName;
+                int busNumber;
+
+                System.out.println("Enter bus Id");
+                busId = ip.nextInt();
+
+                System.out.println("Enter bus Name");
+                busName = ip.next();
+
+                System.out.println("Enter bus Number");
+                busNumber = ip.nextInt();
+
+                bus oBus = new bus(busId, busName, busNumber);
+
+                oBus.addBus(write);
+
+                write.close();
+            } else if (selectioninput == 2) {
+                int customerId;
+                String customerName;
+                int customerPhone;
+                String customerAddr;
+
+                System.out.println("Enter Customer Id");
+                customerId = ip.nextInt();
+
+                System.out.println("Enter Customer Name");
+                customerName = ip.next();
+
+                System.out.println("Enter Customer Phone");
+                customerPhone = ip.nextInt();
+
+                System.out.println("Enter Customer Address");
+                customerAddr = ip.next();
+
+                customer oCust = new customer(customerId, customerName, customerPhone, customerAddr);
+
+                oCust.addCustomer(write1);
+
+                write1.close();
+            }
+
+            else if (selectioninput == 0) {
+                exit = true;
+            }
+        }
     }
 }
