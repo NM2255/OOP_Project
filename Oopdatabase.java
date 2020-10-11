feb4806c220836dbf8a43dd808d269d133ce93e9
MuhammadAhsan12 <ahsan.muh123@gmail.com>
2020-10-11 14:27:12 +0500

    DatabaseComplete

--- Oopdatabase.java
+++ Oopdatabase.java
@@ -20,8 +20,13 @@ public class Oopdatabase {
         System.out.println("Enter 1 for Admin && Enter 2 for User");
         int input = obj.nextInt();
         
+        //===================\\
+        //====>> Admin <<<==== 
+        //===================\\
+        
         if (input == 1){
-        // read Admin data
+            
+        //====>> read Admin data <<====\\
        
         String sql="select * from admin";
         ps=con.prepareStatement(sql);
@@ -31,6 +36,7 @@ public class Oopdatabase {
         int usinput = obj.nextInt();
         
         if(usinput == 1){
+            
         while (rs.next()){
             System.out.print("id : "+rs.getInt(1)+"\t");
             System.out.print("name : "+rs.getString(2)+"\t");
@@ -39,21 +45,23 @@ public class Oopdatabase {
     }
         
         
-        // write admin data
-        else if (usinput == 2){
-            System.out.println("name: ");
-            String name = obj.nextLine();
-            System.out.println("password: ");
-            int pass = obj.nextInt();
-            String add = "insert into user (name1,pass)" + "values ('" + name + "','" + pass + "')";
-            ps = con.prepareStatement(add);
-            ps.executeUpdate();
-        }
+        // =====>> write admin data <<====\\
+        
+    else if (usinput == 2){
+            
+        System.out.println("name: ");
+        String name = obj.nextLine();
+        System.out.println("password: ");
+        int pass = obj.nextInt();
+        String add = "insert into user (name1,pass)" + "values ('" + name + "','" + pass + "')";
+        ps = con.prepareStatement(add);
+        ps.executeUpdate();
+    }
         
             
-         // For Delete
+         //====>> For Delete admin data <<====\\
         
-        else if(usinput ==3){
+    else if(usinput ==3){
             
         int id;
        
@@ -66,11 +74,11 @@ public class Oopdatabase {
         if(r == 1){
             System.out.println("record delete successfully");
         }
-        }
+    }
         
-        // for Edit
+        //=====>> for Edit admin data <<=====\\
         
-        else{
+    else{
         
          int id;
         String name, pasword;
@@ -90,14 +98,15 @@ public class Oopdatabase {
         if(s > 0){
             System.out.println("record Update successfully");
         }
-        }
+    }
          
 }
          
-       
+        //===================\\
         //====>> USER <<<==== 
+        //===================\\
         
-        else{
+    else{
         String sql = "select * from user";
         ps = con.prepareStatement(sql);
         rs = ps.executeQuery();
@@ -107,7 +116,7 @@ public class Oopdatabase {
         
         // Read user data
         
-        if (userinput == 2){
+    if (userinput == 2){
         while (rs.next()) {
             System.out.println("id"+rs.getInt(1)+"\t");
             System.out.println("Name1"+rs.getString(2)+"\t");
@@ -115,10 +124,11 @@ public class Oopdatabase {
             System.out.println("id"+rs.getInt(4)+"\t");
             System.out.println("ticketno"+rs.getString(5)+"\n");
         }
-        }
+    }
         
-            // Write user data
-        else if(userinput == 2){
+            //===>> Write user data <<====\\
+                
+    else if(userinput == 2){
         System.out.println("name: ");
         String name = obj.next();
         System.out.println(" fathername: ");
@@ -132,11 +142,11 @@ public class Oopdatabase {
         "values ('" + name + "','" + father +"','" + nic + "','" + ticketNo + "')";
         ps = con.prepareStatement(add);
         ps.executeUpdate();
-        }
+    }
         
-        // For Delete
+        //===>> For Delete user data <<=====
         
-        else if(userinput ==3){
+    else if(userinput ==3){
             
         int id;
        
@@ -149,14 +159,17 @@ public class Oopdatabase {
         if(q == 1){
             System.out.println("record delete successfully");
         }
-        }
-         else{
+    }
+                
+         //======>> For Edit user data <<=====\\
+                
+    else{
         
          int id, nic;
         String name, fatherName, tickno;
         
         con = DriverManager.getConnection("jdbc:ucanaccess://oopdatabase.accdb");
-        ps = con.prepareStatement("update Admin set name1 = ?, fathername = ?, nic = ?, ticketno = ? where ID = ?");
+        ps = con.prepareStatement("update user set name1 = ?, fathername = ?, nic = ?, ticketno = ? where ID = ?");
         System.out.println("Enter the new name");
         name = obj.next();
         System.out.println("Enter the new Father name");
@@ -176,8 +189,8 @@ public class Oopdatabase {
         if(s > 0){
             System.out.println("record Update successfully");
         }
-        }
-        }
+      }
+    }
         
     }
 }
